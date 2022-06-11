5914b3e0d6f45809133c82f243045637a7237666
janwarlen <janwarlen@foxmail.com>
2022-06-12 00:13:01 +0800

    1. 添加未export类调用案例(直接调用/通过export类调用)

--- service/src/main/java/com/janwarlen/service/ServiceDemo.java
+++ service/src/main/java/com/janwarlen/service/ServiceDemo.java
@@ -1,10 +1,14 @@
 package com.janwarlen.service;
 
 import com.janwarlen.modular.common.StringUtil;
+// Package 'com.janwarlen.modular.intern' is declared in module 'common', which does not export it to module 'service'
+//import com.janwarlen.modular.intern.PrivateUtil;
 
 public class ServiceDemo {
 
     public static void main(String[] args) {
         StringUtil.hello();
+        StringUtil.test();
+//        PrivateUtil.access();
     }
 }
